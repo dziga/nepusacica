c8837eb14429bf4b8702257ee84b73ee6e909b63
Nikola Milivojevic <nikola@codehouse.(none)>
2012-12-01 17:52:50 +0100

    Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nepusacica::Application.config.secret_token = 'cd8a0533b399226daf72d28685ff37b98538ee779ff18dcc1a382ed392828b3cf81b4dab252b518ba3e913be750dbfdf91d635bb66d49e14b4b1a3d3f4da15ef'
