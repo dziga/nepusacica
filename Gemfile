ec3c2ea13cda367000d2dcd82bf7c915ac3b7d19
Nikola Milivojevic <nikola@codehouse.(none)>
2012-12-01 19:15:14 +0100

    adding bootstrap

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ group :assets do
 end
 
 gem 'jquery-rails'
+gem 'sass-twitter-bootstrap-rails', '~>1.0'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(:version => 20121201174902) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "caves", :force => true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "opinions", :force => true do |t|
     t.integer  "caffe_id"
     t.text     "comment"
